ac594a9bec2eddf8a5cf6df8a7502c108c076ec5
Roger Llopart Pla <lumbendil@gmail.com>
2013-04-02 22:31:03 +0000

    Afegits camps adicionals a l'entitat "Movie".

---
+++ application/app/DoctrineMigrations/Version20130402222930.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130402222930 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE movie ADD year SMALLINT NOT NULL, ADD runtime SMALLINT NOT NULL, ADD criticsConsensus VARCHAR(255) DEFAULT NULL, ADD rottenTomatoesId INT DEFAULT NULL, ADD synopsis VARCHAR(1024) DEFAULT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE movie DROP year, DROP runtime, DROP criticsConsensus, DROP rottenTomatoesId, DROP synopsis");
+    }
+}
